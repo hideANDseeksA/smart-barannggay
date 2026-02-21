2e6bfe58177f1168376fe3307855fca05dd3aa2c
sleepyProgrammer17 <jhonbraynrafe@gmail.com>
2026-02-21 13:30:43 +0800

    update server

--- src/server.ts
+++ src/server.ts
@@ -26,7 +26,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: ["http://localhost:5173", "https://front-end-thesis-vert.vercel.app"],
     credentials: true,              
   })
 );
